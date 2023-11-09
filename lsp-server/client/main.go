481806712a2f81ac1cf51fac837a46dce001f03f
Adhyan H <adhyanpatel@gmail.com>
2023-11-09 03:14:59 -0600

    make binary node be replaced by the operator

--- lsp-server/client/main.go
+++ lsp-server/client/main.go
@@ -4,6 +4,7 @@ import (
     "bytes"
     "encoding/json"
     "fmt"
+    "strings"
     "net/http"
 )
 
@@ -17,12 +18,13 @@ const (
     Object                    
     Function                  
     None                      
+    Operator
 )
 
 func (s SuneidoType) String() string {
 	return [...]string{
         "Unknown", "Any", "Boolean", "Number", "String", 
-        "Object", "Function", "None"}[s]
+        "Object", "Function", "None", "Operator"}[s]
 }
 
 type Node struct {
@@ -31,46 +33,10 @@ type Node struct {
     Type     SuneidoType
 }
 
-func Parse(input string) Node {
+func generateAST(input string) Node {
 	stack := []Node{}
 	word := ""
-
-	for _, c := range input {
-		switch c {
-		case '(':
-			if word != "" {
-				stack = append(stack, Node{Value: word})
-				word = ""
-			}
-			stack = append(stack, Node{Value: "("})
-		case ')':
-			if word != "" {
-				stack = append(stack, Node{Value: word})
-				word = ""
-			}
-			temp := []Node{}
-			for len(stack) > 0 && stack[len(stack)-1].Value != "(" {
-				temp = append(temp, stack[len(stack)-1])
-				stack = stack[:len(stack)-1]
-			}
-			stack = stack[:len(stack)-1] // pop "("
-			stack[len(stack)-1].Children = temp
-		case ' ':
-			if word != "" {
-				stack = append(stack, Node{Value: word})
-				word = ""
-			}
-		default:
-			word += string(c)
-		}
-	}
-
-	return stack[0]
-}
-
-func Parse2(input string) Node {
-	stack := []Node{}
-	word := ""
+    word = strings.TrimSpace(word)
 
 	for _, c := range input {
 		switch c {
@@ -117,7 +83,7 @@ func printNode(node Node, indent string) {
 func main() {
     // Make an HTTP POST request to the server
     inputStr := "function() { x = 1 }"
-    inputStr = "function() { foo = 123; if (foo) { return 'hello' } else { return true } }"
+    // inputStr = "function() { foo = 123; if (foo) { return 'hello' } else { return true } }"
     input := RequestJSON{Input: inputStr}
     responseJSON, err := sendRequest(input, "http://localhost:8080/process")
     if err != nil {
@@ -127,7 +93,7 @@ func main() {
 
     response := responseJSON.Output
     fmt.Println("response: ", response)
-    parsedAst := Parse2(response)
+    parsedAst := generateAST(response)
     printNode(parsedAst, "")
     // fmt.Println("parsed: ", printNode(Parse2(response), ""))
 }
@@ -159,3 +125,4 @@ type RequestJSON struct {
 type ResponseJSON struct {
     Output string `json:"output"`
 }
+
