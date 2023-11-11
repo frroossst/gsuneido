13efc2677f3d979a07846ffcd0e5c9e1c6b3b810
Adhyan H <adhyanpatel@gmail.com>
2023-11-11 01:11:50 -0600

    use inbuilt lexer and parser

--- lsp-server/client/main.go
+++ lsp-server/client/main.go
@@ -6,14 +6,32 @@ import (
     "fmt"
     "strings"
     "net/http"
+
+	tok "github.com/apmckinlay/gsuneido/compile/tokens"
+	"github.com/apmckinlay/gsuneido/compile/lexer"
 )
+
+type TypeError int
+const (
+    TypeMismatch TypeError = iota
+    TypeNotKnown
+    TypeNotInferred
+    TypeNotInferredFromChildren
+    TypeNotInferredFromParent
+    TypeNotInferredFromSibling
+)
+
+type TypeErrorThrown struct {
+    ErrorType TypeError
+    Message string
+}
 
 type SuneidoType int
 const (
     Unknown SuneidoType = iota
     Any                       
     Boolean                   
-    Number                   
+    Number                    
     String                    
     Object                    
     Function                  
@@ -25,6 +43,17 @@ func (s SuneidoType) String() string {
 	return [...]string{
         "Unknown", "Any", "Boolean", "Number", "String", 
         "Object", "Function", "None", "Operator"}[s]
+}
+
+type EqualTypeOperators int
+const (
+    Eq EqualTypeOperators = iota
+    Cat
+)
+type OperatorType struct {
+    Operator EqualTypeOperators
+    Left     SuneidoType
+    Right    SuneidoType
 }
 
 type Node struct {
@@ -132,10 +161,53 @@ func directTypeInference(node Node, parent Node) Node {
 func main() {
     // Make an HTTP POST request to the server
     inputStr := "function() { x = 1 }"
+    // inputStr = "function(a, b) { return a is b  }"
+    // inputStr = "function() { return 1 + 2 + 3 + 4 + 5 + 6 }"
+    inputStr = 
+    `function(x) 
+        {
+        foo = "123"
+        bar = x $ foo
+        baz = foo is bar
+        return baz
+        }
+    `
     // inputStr = "function() { x = 'foo'\n if String?(foo) { return 'str' } }"
     // inputStr = "function() { x = Object(a: 1, b: 's') }"
     // inputStr = "function() { x++ }"
     // inputStr = "function() { foo = 123; if (foo) { return 'hello' } else { return true } }"
+    
+    // ! testing official compiler
+    // define an arrray of tokens to skip
+    skipPrint := []tok.Token{tok.Whitespace, tok.Newline, tok.Comment, tok.LCurly, tok.RCurly, tok.LParen, tok.RParen}
+    skipPrintMap := make(map[tok.Token]bool)
+    for _, token := range skipPrint {
+        skipPrintMap[token] = true
+    }
+
+    lxr := lexer.NewLexer(inputStr)
+    for ;; {
+        item := lxr.Next()
+        if item.Token == tok.Eof {
+            break
+        }
+        if !skipPrintMap[item.Token] {
+            fmt.Println(item)
+        }
+    }
+    //  var mainThread Thread
+    //	mainThread.Name = "main"
+    //	mainThread.UIThread = true
+    //	v, results := compile.Checked(&mainThread, inputStr)
+    //    fmt.Println("v: ", v)
+    //	for _, s := range results {
+    //		fmt.Println("(" + s + ")")
+    //	}
+	// p := compile.AstParser(inputStr)
+    if true {
+        panic("[DEBUG]")
+    }
+
     input := RequestJSON{Input: inputStr}
     responseJSON, err := sendRequest(input, "http://localhost:8080/process")
     if err != nil {
@@ -147,17 +219,22 @@ func main() {
     fmt.Println("response: ", response)
     parsedAst := generateAST(response)
     newRoot := reduceTree(parsedAst)
-    fmt.Println("\n")
+    fmt.Println()
     printNode(newRoot, "\t")
 }
 
-func printNode(node Node, indent string) {
+func printNode(node Node, indent string) error {
 	fmt.Println(indent + "Node: " + node.Value)
 	fmt.Println(indent + "  Type: " + node.Type.String())
+    if len(node.Children) == 0 {
+        fmt.Println(indent + "  Children: []")
+        return nil
+    }
 	fmt.Println(indent + "  Children: ")
 	for _, child := range node.Children {
 		printNode(child, indent+"    ")
 	}
+    return nil
 }
 
 func sendRequest(input RequestJSON, url string) (ResponseJSON, error) {
